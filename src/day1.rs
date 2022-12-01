de134309dc9d042ae1b9a0e129e461223f17a9f9
Mateusz Leonowicz <mleonowicz@internships.antmicro.com>
2022-12-01 22:02:53 +0100

    Introduce map iterators

--- src/day1.rs
+++ src/day1.rs
@@ -1,40 +1,23 @@
 #[aoc(day1, part1)]
 fn solve_part1(input: &str) -> i32 {
-    let mut count = 0;
-    let mut max = -1;
-    for line in input.lines() {
-        if !line.trim().is_empty() {
-            count += line.parse::<i32>().unwrap();
-        }
-        else {
-            if count > max {
-                max = count
-            }
-            count = 0;
-        }
-    }
-    if count > max {
-        max = count
-    }
-
-    max
+    input
+        .split("\n\n")
+        .map(|l|
+            l.lines().map(|n|
+                n.parse::<i32>().unwrap()
+            ).sum()
+        ).max().unwrap()
 }
 
 #[aoc(day1, part2)]
 fn solve_part2(input: &str) -> i32 {
-    let mut calories: Vec<i32> = Vec::new();
-    let mut count = 0;
-    for line in input.lines() {
-        if !line.trim().is_empty() {
-            count += line.parse::<i32>().unwrap();
-        }
-        else {
-            calories.push(count);
-            count = 0;
-        }
-    }
-    calories.push(count);
-    calories.sort();
-
-    calories[calories.len() - 1] + calories[calories.len() - 2] + calories[calories.len() - 3]
+    let mut res: _ = input
+        .split("\n\n")
+        .map(|l|
+            l.lines().map(|n|
+                n.parse::<i32>().unwrap()
+            ).sum()
+        ).collect::<Vec<i32>>();
+    res.sort();
+    res.iter().rev().take(3).sum::<i32>()
 }
